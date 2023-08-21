0929e653fbf04b7baebc64eb1ec86d134b205239
co <dx.cobang@gmail.com>
2023-08-20 20:39:37 -0400

    fix: Remove console

--- src/policies/Leverager_Audit.sol
+++ src/policies/Leverager_Audit.sol
@@ -2,8 +2,6 @@
 pragma solidity 0.8.15;
 pragma abicoder v2;
 
-import "forge-std/console2.sol";
-
 import {IERC20, IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
@@ -347,8 +345,6 @@ contract Leverager is
                     ++i;
                 }
             }
-
-            console2.log("------- Reward: ", reward);
 
             if (reward == 0) return;
 
