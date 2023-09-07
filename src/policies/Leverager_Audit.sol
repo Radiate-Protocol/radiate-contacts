93b35afbb04f89380262aca9410a875773f10b4e
co <dx.cobang@gmail.com>
2023-09-07 13:41:14 -0400

    fix: Issue #40 Typographical issues

--- src/policies/Leverager_Audit.sol
+++ src/policies/Leverager_Audit.sol
@@ -2,7 +2,7 @@
 pragma solidity 0.8.15;
 pragma abicoder v2;
 
-import {IERC20, IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
 import {EnumerableSet} from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
@@ -132,7 +132,6 @@ contract Leverager is
 
     event KernelChanged(address kernel);
     event DistributorChanged(address distributor);
-    event BorrowRatioUpdated(uint256 borrowRatio);
     event HardCapChanged(uint256 aHardCap);
     event MinStakeAmountChanged(uint256 minStakeAmount);
     event Staked(address indexed account, uint256 amount);
